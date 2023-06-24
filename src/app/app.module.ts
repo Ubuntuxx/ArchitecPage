b081e2a77269d0b4ca01746307c364ba4eb6fc14
JLAGUbuntu <unixluisx@protonmail.ch>
2023-06-24 16:48:11 -0600

    proyectos y sobre la empresa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LogoComponent } from './logo/logo.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoComponent
+    LogoComponent,
+    MainContentComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule
